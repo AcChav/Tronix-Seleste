96e8df46711eb3f3740570d345dab6c06c7f7d7d
AcChav <albrechtchavez50@gmail.com>
2023-08-08 10:31:00 +0800

    .

--- slick-slider.js
+++ slick-slider.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
       {
         breakpoint: 991,
         settings: {
-          slidesToShow: 3
+          slidesToShow: 2
         }
       },
       {
--- slider.js
+++ slider.js
@@ -18,7 +18,7 @@ $(document).ready(function(){
       {
         breakpoint: 991,
         settings: {
-          slidesToShow: 3
+          slidesToShow: 2
         }
       },
       {
